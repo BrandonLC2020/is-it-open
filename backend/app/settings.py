e14d4b3dd32e4e5dbab2f9ec04c57c9c78160f12
Brandon Lamer-Connolly <brandon.lc.1334@gmail.com>
2026-02-14 13:46:31 -0500

    update db config

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -78,11 +78,14 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+import dj_database_url
+from decouple import config
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL', default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        conn_max_age=600
+    )
 }
 
 
